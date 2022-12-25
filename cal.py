f71423ac8c11036247b1d9f216f10e9b389f2acf
Bipin Uprety <86381942+MrAAGO@users.noreply.github.com>
2022-12-25 13:54:16 -0800

    Create cal.py

---
+++ cal.py
@@ -0,0 +1,26 @@
+# Function to perform the desired operation
+def calculate(num1, num2, operator):
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        return num1 / num2
+
+# Main program
+while True:
+    # Get user input
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    operator = input("Enter operator (+, -, *, /): ")
+
+    # Perform calculation and print result
+    result = calculate(num1, num2, operator)
+    print("Result:", result)
+
+    # Prompt user to continue or exit
+    cont = input("Do you want to perform another calculation? (y/n): ")
+    if cont.lower() != "y":
+        break
